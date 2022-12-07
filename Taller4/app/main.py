fe6ca3f612c7060e3e3914f3d0a93a0a7b1fe2ba
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2022-12-06 22:55:29 -0500

    Update main.py

--- Taller4/app/main.py
+++ Taller4/app/main.py
@@ -22,7 +22,12 @@ data_pre = pd.read_json('https://raw.githubusercontent.com/JulianTorrest/MINE-Ci
 data_train_1 = pd.read_json(
     'https://raw.githubusercontent.com/JulianTorrest/MINE-Ciencia-de-datos-Aplicada/main/Taller4/data/DataSet_Entrenamiento_v1.json')
 
-st.title('Taller 4, Tasa Churn')
+st.title('Taller 4, churn Rate')
+
+url = 'https://raw.githubusercontent.com/JulianTorrest/MINE-Ciencia-de-datos-Aplicada/main/Taller4/model/Modelo.pkl'
+response = requests.get(url)
+open("Modelo.pkl", "wb").write(response.content)
+best_model = joblib.load("Modelo.pkl")
 
 
 def load_pred():
@@ -35,7 +40,7 @@ def load_pred():
 
 
 def cleaning(dataset):
-    # Limpiar la columna TotalCharges con 0
+    # se limpia la columna TotalChares que tiene problemas con valores 0
     dataset.loc[dataset["tenure"] == 0, "TotalCharges"] = "0"
     dataset['TotalCharges'].astype(float)
     # se asocian por tipo las columnas
@@ -49,14 +54,14 @@ def cleaning(dataset):
     ]]
     excluidas = dataset[["customerID", "MultipleLines"]]
 
-    # Realizar transformación
+    # se hace la transformación
     objetivo = objetivo.replace(['No', 'Yes'], [0, 1])
     gender = gender.replace(['Female', 'Male'], [0, 1])
     categoricas_1 = categoricas_1.replace(['No', 'Yes'], [0, 1])
     categoricas_2 = pd.get_dummies(categoricas_2)
     numericas = numericas.astype(float)
 
-    # Dataset limpio
+    # se construye todo el dataset limpio de nuevo
     clean_dataset = pd.DataFrame().join([objetivo, gender, categoricas_1, categoricas_2, numericas], how="outer")
     return clean_dataset
 
@@ -75,13 +80,13 @@ def cleaning_1(dataset):
     ]]
     excluidas = dataset[["customerID", "MultipleLines"]]
 
-    # Realizar la transformación
+    # se hace la transformación
     gender = gender.replace(['Female', 'Male'], [0, 1])
     categoricas_1 = categoricas_1.replace(['No', 'Yes'], [0, 1])
     categoricas_2 = pd.get_dummies(categoricas_2)
     numericas = numericas.astype(float)
 
-    # Limpiar dataset
+    # se construye todo el dataset limpio de nuevo
     clean_dataset = pd.DataFrame().join([gender, categoricas_1, categoricas_2, numericas], how="outer")
     return clean_dataset
 
@@ -90,7 +95,7 @@ def get_final_pred_mv0(dataset, model):
     # se limpia para que pueda ser ingerido por el modelo
     clean_df3 = cleaning_1(dataset)
 
-    # Pronostico con el primer modelo
+    # se hace la predicción con el primer modelo
     df_predicted = pd.DataFrame(model.predict(clean_df3)).replace([0, 1], ['No', 'Yes'])
     df_precited_proba = pd.DataFrame(model.predict_proba(clean_df3)[:, 1])
     df_predicted["proba"] = df_precited_proba
@@ -123,7 +128,7 @@ def reentrenamiento(df1, df2):
         "logistic__solver": ['liblinear', 'saga'],
     }
 
-    # Buscar el mejor modelo
+    # se busca el mejor modelo y regresa el score
     logistic_rtmodel = GridSearchCV(pipe, param_grid, n_jobs=2, scoring='roc_auc', cv=5).fit(X, Y)
     # score_logistic = roc_auc_score(Y_test, logistic_model.predict_proba(X_test)[:, 1])
 
@@ -132,39 +137,57 @@ def reentrenamiento(df1, df2):
     return logistic_rtmodel
 
 
-if st.checkbox('Utilizar el primer modelo'):
+if st.checkbox('check for use first model'):
     # load model
-    url = 'https://raw.githubusercontent.com/JulianTorrest/MINE-Ciencia-de-datos-Aplicada/main/Taller4/model/Modelo.pkl'
-    response = requests.get(url)
-    open("Modelo.pkl", "wb").write(response.content)
-    best_model = joblib.load("Modelo.pkl")
-    uploaded_file = st.file_uploader(label='Datos cargados para el pronostico')
+    uploaded_file = st.file_uploader(label='upload dataset for prediction')
     final_d = ''
     if uploaded_file is not None:
-        data_prediction = pd.read_json(uploaded_file.getvalue())
-        prediction = get_final_pred_mv0(data_prediction, best_model)
-        print("final prediction", prediction)
+        data_predi = pd.read_json(uploaded_file.getvalue())
+        prediction = get_final_pred_mv0(data_predi, best_model)
+        # print("final prediction", prediction)
         final_d = st.dataframe(prediction)
-        st.write(f"Your churn results: {final_d}")
+        # st.write(f"Your churn results: {final_d}")
 
-if st.checkbox('Utilizar el segundo modelo'):
+if st.checkbox('check for use second model'):
     uploaded_file = st.file_uploader(label='upload dataset for training')
     final_d = ''
     if uploaded_file is not None:
         data_2_train = pd.read_json(uploaded_file.getvalue())
         model_2 = reentrenamiento(data_2_train, data_train_1)
-        uploaded_file = st.file_uploader(label='Datos cargados para el pronostico')
+        uploaded_file = st.file_uploader(label='upload dataset for predictions')
         if uploaded_file is not None:
-            prediction1 = pd.read_json(uploaded_file.getvalue())
-            prediction2 = get_final_pred_mv0(prediction1, model_2)
+            pred = pd.read_json(uploaded_file.getvalue())
+            prediction2 = get_final_pred_mv0(pred, model_2)
             print("final prediction", prediction2)
             final_d = st.dataframe(prediction2)
-            st.write(f"Your churn results: {final_d}")
+            # metrics
+            clean_df2 = cleaning(data_2_train)
+            clean_df1 = cleaning(data_train_1)
+            rtrain_df = pd.concat([clean_df1, clean_df2], axis=0)
 
-# if st.button('Realizar pronostico con nuevo modelo'):
+            X_last = rtrain_df.drop("Churn", axis=1)
+            Y_last = rtrain_df["Churn"]
+            X_train, X_test, Y_train, Y_test = train_test_split(X_last, Y_last, test_size=0.3, stratify=Y_last,
+                                                                random_state=33)
+            predictions_v1 = best_model.predict(X_test)
 
-# model_2 = reentrenamiento(data_2_train, data_train_1)
-# prediction2 = get_final_pred_mv0(data_pre, model_2)
-# print("final prediction", np.squeeze(prediction2, -1))
-# final_d2 = pd.DataFrame.to_string(prediction2)
-# st.write(f"Your churns: {final_d2}")
+            print(classification_report(Y_test, predictions_v1))
+            a = classification_report(Y_test, predictions_v1)
+            df_a = pd.DataFrame(a)
+            st.dataframe(df_a)
+            print(roc_auc_score(Y_test, best_model.predict_proba(X_test)[:, 1]))
+            b = roc_auc_score(Y_test, best_model.predict_proba(X_test)[:, 1])
+            st.write(f"Your original roc_auc_score: {b}")
+
+            # segundo modelo
+
+            predictions_v2 = model_2.predict(X_test)
+
+            print(classification_report(Y_test, predictions_v2))
+            c = classification_report(Y_test, predictions_v2)
+            df_c = pd.DataFrame(c)
+            st.dataframe(df_c)
+            print(roc_auc_score(Y_test, model_2.predict_proba(X_test)[:, 1]))
+            d = roc_auc_score(Y_test, model_2.predict_proba(X_test)[:, 1])
+            # df_d = pd.DataFrame(d).transpose()
+            st.write(f"Your new roc_auc_score: {d}")
